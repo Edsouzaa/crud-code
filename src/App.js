95aebacb97b588cd9a99ffd59f19ee761782afc7
Edsouzaa <edsonhenriqueabreu@gmail.com>
2025-10-06 22:49:15 -0300

    mtas coisas

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import './App.css';
 
 // Importar os componentes
 import Login from './components/Login/Login.js';
+import Menu from './components/Menu/Menu.js';
+import Welcome from './components/Welcome/Welcome.js';
+import ContactList from './components/ContactList/ContactList.js';
+import ContactForm from './components/ContactForm/ContactForm.js';
 
 function App(){
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [activeScreen, setActiveScreen] = useState('Welcome');
+  const [contacts, setContacts] = useState([
+    {id:1,name:'Tatiana Silva',phone:'1621068702'},
+    {id:2,name:'Graziela Carozelli',phone:'1621068702'}]);
+
+  const [contactToEdit, setContactToEdit] = useState(null);
 
   const handleLogin = (username, password) => {
     if (username === 'admin' && password === '123'){
@@ -15,11 +25,32 @@ function App(){
     }
   };
 
+  const handleSaveContact = (contact) => {}
+  const handleDeleteContact = (id) => {}
+  const startEdit = (contact) => {}
+  const showCreatForm = () => {}
+  const handleNavigate = () => {}
+
   if (!isLoggedIn){
     return(
     <Login onLogin={handleLogin}/>
     )
   }
+  return(
+    <div>
+      <Menu onNavigate={handleNavigate} onCreate={showCreatForm}/>
+      <main className='content'>
+        {activeScreen === 'Welcome' && <Welcome/>}
+        {activeScreen === 'list' && <ContactList 
+                                    contacts={contacts} 
+                                    onEdit={startEdit} 
+                                    onDelete={handleDeleteContact}/>}
+        {activeScreen === 'form' && <ContactForm 
+                                    contactToEdit={contactToEdit}  
+                                    onSave={handleSaveContact}/>}
+      </main>
+    </div>
+  )
 
 };
 export default App;
---
+++ src/components/ContactList/ContactList.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import './ContactList.css';
+
+function ContactList({contacts, onEdit, onDelete}){
+    return(
+        <div className='list-container'>
+            <h2>Lista de Contatos</h2>
+            {contacts.lenght > 0 ? 
+            (
+                <ul className='contact-list'>
+                    {contacts.map(contact => (
+                        <li className='contact-item' key={(contact.id)}>
+                            <div className='contact-info'>
+                                <span className='contact-name'>{contact.name}</span>
+                                <span className='contact-phone'>{contact.phone}</span>
+                            </div>
+                            <div className='contact-actions'>
+                                <button className='edit-btn' onClick={()=>onEdit(contact)}>Alterar</button>
+                                <button className='delete-btn' onClick={()=>onDelete(contact.id)}>Deletar</button>
+                            </div>
+                        </li>
+                    ))}
+                </ul>):(<p className='no-contacts'>Nenhum contato cadastrado</p>)}
+        </div>
+    )
+}
+export default ContactList;
